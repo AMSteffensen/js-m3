26e5e35a6f2b9890fd125c5d89b14f846cb7c30e
amsteffensen <andreassteffensen@Andreass-MacBook-Air.local>
2018-09-23 23:12:22 +0200

    done

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,11 +1,18 @@
 //Use RegEx to validate form
 
 function valdidateForm(){
- 
+    
+    // create variables for elements
     var firstName = document.getElementById('fname').value;
     var lastName = document.getElementById('lname').value;
     var telePhone = document.getElementById('telNo').value;
     var emailAdress = document.getElementById('email').value;
+
+     // create a regex patterns
+    var emailRegEx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    var fNameRegEx = /[a-zA-Z]{2,4}/i;
+    var lNameRegEx = /[a-zA-Z]{2,4}/i;
+    var telNoRegEx = /[0-9]{8}/;
 
 
     //valdidate that fields are not empty
@@ -21,15 +28,6 @@ function valdidateForm(){
         console.log("sucess!");
     }
 
-    // output messages
-    var alertMsg = 'Wrong email adress'
-
-    // create a pattern to test an email address
-    var emailRegEx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    var fNameRegEx = /^[0-9]+$/;
-    var lNameRegEx = /^[0-9]+$/;
-    var telNoRegEx = /^[0-9]+$/;
-
     // valdidate email adress
     if(emailRegEx.test(emailAdress)) {
         document.getElementById('email').style.backgroundColor = 'green';
@@ -39,15 +37,14 @@ function valdidateForm(){
     }
 
     // valdidate phone number
-    if(telNoRegEx.test(telNo)) {
+    if(telNoRegEx.test(telePhone)) {
         document.getElementById('telNo').style.backgroundColor = 'green';
     } else {
         document.getElementById('telNo').style.backgroundColor = 'red';
-        console.log("Number incorrect")
     }
 
     // valdidate last name
-    if(lNameRegEx.test(lname)) {
+    if(lNameRegEx.test(lastName)) {
         document.getElementById('lname').style.backgroundColor = 'green';
     } else {
         document.getElementById('lname').style.backgroundColor = 'red';
@@ -55,16 +52,16 @@ function valdidateForm(){
     }
 
      // valdidate first name
-     if(fNameRegEx.test(fname)) {
+     if(fNameRegEx.test(firstName)) {
         document.getElementById('fname').style.backgroundColor = 'green';
     } else {
         document.getElementById('fname').style.backgroundColor = 'red';
-        console.log("Number incorrect")
+        alert("Number incorrect")
     }
-
-
-
-
 }
+
+
+
+
 
 
--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,56 @@
 //Console.log out elements in JSON file
+
+var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(xhttp.responseText);
+            var video = response.video;
+    
+            var output = '';
+
+            for(var i = 0; i < video.length; i++) {
+            
+              output += '<li>'+video[i].id+'</li>' + 
+              '<li>'+video[i].name+'</li>'  + 
+              '<li>'+video[i].url+'</li>';
+              
+              console.log(video[0].id);
+              console.log(video[0].name);
+              console.log(video[0].url);
+              console.log(video[0].author);
+                
+            var data = video[0].author.data;
+            var dataOutput = '';
+
+            }
+            document.getElementById('video').innerHTML = output;
+            
+          }
+      };
+      xhttp.open("GET", "author.json", true);
+      xhttp.send();
+
+
+    
+
+/*
+
+      {
+        "video": [{
+            "id": 12312412312,
+            "name": "Ecuaciones Diferenciales",
+            "url": "/video/math/edo/12312412312",
+            "author": {
+                "data": [{
+                    "name_author": "Alejandro Morales",
+                    "url": "/author/alejandro-morales",
+                    "type": "master"
+                }]
+            }
+        }]
+    }
+
+    */
+
+
+    
